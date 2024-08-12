87b440dd6df572b1d0901e5fa1d87bfeabc5c415
Carlos <jose.carlosnascimento@hotmail.com>
2024-08-12 12:59:02 -0300

    teste python

---
+++ pythonProject2/teste.py
@@ -0,0 +1,19 @@
+numero = float (input('Digite um número:'))
+
+dobro = numero * 2
+
+print (f'O dobro de {numero} é {dobro}')
+
+a = 2
+b = 3
+
+print (a + b )
+
+
+print ('Calculadora')
+num1 = int(input('Insira o primeiro número'))
+num2 = int(input('Insira o segundo numero'))
+
+resultado = num1 + num2
+
+print (resultado)
